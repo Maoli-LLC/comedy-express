6508d59ffa45e57f3e42a5c5f72e735094016252
globalxbank@gmail.com <globalxbank@gmail.com>
2020-12-30 20:13:19 -0500

    updating to use lambda

--- routes/videosApi.js
+++ routes/videosApi.js
@@ -7,10 +7,12 @@ const app = express();
 const router = express.Router();
 
 router.get('/', (req, res) => {
+
     axios.get('https://muse.ai/api/files/collections?metadata=full', {
         headers: {
-            'Key': '8a3BSnS90pYiSZP2lEVYNUFo5fdcf818'
-  
+            'Key': '8a3BSnS90pYiSZP2lEVYNUFo5fdcf818',
+            "Access-Control-Allow-Origin": "*",
+            'Access-Control-Allow-Methods' : 'GET,PUT,POST,DELETE,PATCH,OPTIONS'
         },
     }).then(resp => {
         res.send(resp.data);
